6e1e62a8eb693cfab85b7a09b45b41362f4e2cf6
VaishNikam <nikamvaishu99@gmail.com>
2026-01-31 23:05:17 +0530

    new file added

---
+++ Ternary_operator_questions/FunctionFindSquare.java
@@ -0,0 +1,13 @@
+/*
+9. Find Square of a Number
+
+Problem: Write a function that returns the square of a given number.
+
+Input:
+n = 9
+
+Output:
+81
+*/
+import java.util.Scanner;
+class FunctionFindSquare
